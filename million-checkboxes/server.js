5a5470aa613c3c20367127f330e7764fa9149f71
Surya Prakash <surya.jinagam@gmail.com>
2026-07-11 18:58:06 +0530

    feat: implement toast notifications for rate limiting and enhance checkbox state handling

--- million-checkboxes/server.js
+++ million-checkboxes/server.js
@@ -8,6 +8,7 @@ import { publisher, subscriber, redis } from "./redis-connection.js";
 
 const CHECK_BOX = 100;
 const REDIS_KEY = "checkbox-key";
+const rateLimitingKey = "rate-limit";
 
 async function main() {
   const PORT = process.env.PORT ?? 8000;
@@ -19,10 +20,10 @@ async function main() {
   io.attach(httpServer);
 
   await subscriber.subscribe("internal-server:checkbox");
- 
+
   subscriber.on("message", async (channel, data) => {
     if (channel === "internal-server:checkbox") {
-      const { id, checked } = JSON.parse(data);     
+      const { id, checked } = JSON.parse(data);
       io.emit("server:checkbox", { id, checked });
     }
   });
@@ -36,7 +37,7 @@ async function main() {
     if (existingData) {
       const remoteData = await JSON.parse(existingData);
       return res.json({ checkbox: remoteData });
-    } else {    
+    } else {
       return res.json({ checkbox: new Array(CHECK_BOX).fill(false) });
     }
   });
@@ -45,17 +46,29 @@ async function main() {
     console.log(`Socket ${socket.id} connected`);
 
     socket.on("client:checkbox", async (data) => {
-      const existingData = await redis.get(REDIS_KEY);  
+      const latestOperation = await redis.get(`${rateLimitingKey}:${socket.id}`);
+      if (latestOperation) {
+        const elapsedTime = Date.now() - latestOperation;
+        if (elapsedTime < 5.5 * 1000) {
+          socket.emit("server:rate-limit", { message: "Please wait." });
+          return;
+        }
+      }
+      await redis.set(`${rateLimitingKey}:${socket.id}`, Date.now());
+
+      const existingData = await redis.get(REDIS_KEY);
       if (existingData) {
         const remoteData = JSON.parse(existingData);
         remoteData[data.id] = data.checked;
         await redis.set(REDIS_KEY, JSON.stringify(remoteData));
       } else {
-        await redis.set(REDIS_KEY,JSON.stringify(new Array(CHECK_BOX).fill(false)));
+        await redis.set(
+          REDIS_KEY,
+          JSON.stringify(new Array(CHECK_BOX).fill(false)),
+        );
       }
       await publisher.publish("internal-server:checkbox", JSON.stringify(data));
     });
-
   });
 
   httpServer.listen(PORT, () => {
@@ -64,3 +77,10 @@ async function main() {
 }
 
 main();
+
+
+
+
+
+
+
