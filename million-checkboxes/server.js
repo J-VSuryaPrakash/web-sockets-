370a52a22a7f19a57df3c7a1e1ee2c84400011e0
Surya Prakash <surya.jinagam@gmail.com>
2026-07-11 12:35:11 +0530

    feat: add Redis support to checkbox state synchronization across multiple servers

--- million-checkboxes/server.js
+++ million-checkboxes/server.js
@@ -1,48 +1,66 @@
-import http from 'node:http';
-import path from 'node:path';
+import http from "node:http";
+import path from "node:path";
 
-import express from 'express';
-import { Server } from 'socket.io';
+import express from "express";
+import { Server } from "socket.io";
+
+import { publisher, subscriber, redis } from "./redis-connection.js";
 
 const CHECK_BOX = 100;
-const store = {
-  checkbox : new Array(CHECK_BOX).fill(false)
-}
+const REDIS_KEY = "checkbox-key";
 
-async function main(){
-
+async function main() {
   const PORT = process.env.PORT ?? 8000;
   const app = new express();
 
   const httpServer = http.createServer(app);
   const io = new Server();
-  
-  io.attach(httpServer)
-  
-  app.use(express.static(path.resolve('./public')))
-  app.get('/health', (req, res) => {
-    res.send({'type': 'health-check'});
-  })
-  app.get('/checkboxes', (req, res) => {
-    return res.json({checkbox: store.checkbox});
-  })
 
+  io.attach(httpServer);
 
-  io.on('connection', (socket) => {
+  await subscriber.subscribe("internal-server:checkbox");
+ 
+  subscriber.on("message", async (channel, data) => {
+    if (channel === "internal-server:checkbox") {
+      const { id, checked } = JSON.parse(data);     
+      io.emit("server:checkbox", { id, checked });
+    }
+  });
+
+  app.use(express.static(path.resolve("./public")));
+  app.get("/health", (req, res) => {
+    res.send({ type: "health-check" });
+  });
+  app.get("/checkboxes", async (req, res) => {
+    const existingData = await redis.get(REDIS_KEY);
+    if (existingData) {
+      const remoteData = await JSON.parse(existingData);
+      return res.json({ checkbox: remoteData });
+    } else {    
+      return res.json({ checkbox: new Array(CHECK_BOX).fill(false) });
+    }
+  });
+
+  io.on("connection", (socket) => {
     console.log(`Socket ${socket.id} connected`);
 
-    socket.on('client:checkbox', (data) => {
-      const {id, checked} = data;
-      store.checkbox[id] = checked;
-      io.emit('server:checkbox', data);
-    })
+    socket.on("client:checkbox", async (data) => {
+      const existingData = await redis.get(REDIS_KEY);  
+      if (existingData) {
+        const remoteData = JSON.parse(existingData);
+        remoteData[data.id] = data.checked;
+        await redis.set(REDIS_KEY, JSON.stringify(remoteData));
+      } else {
+        await redis.set(REDIS_KEY,JSON.stringify(new Array(CHECK_BOX).fill(false)));
+      }
+      await publisher.publish("internal-server:checkbox", JSON.stringify(data));
+    });
 
-  })
+  });
 
   httpServer.listen(PORT, () => {
-    console.log(`Server is running http://localhost:${PORT}`)
-  })
-
+    console.log(`Server is running http://localhost:${PORT}`);
+  });
 }
 
 main();
